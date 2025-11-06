65765944e90f96c87a624077f3ac8ad6b576f6fb
Joaquim Moraes <joaquimsp2006@gmail.com>
2025-11-06 20:19:45 -0300

    Adicionando Client

---
+++ src/main/java/com/example/Denuncia_Service/Client/UsuarioClient.java
@@ -0,0 +1,18 @@
+package com.example.Denuncia_Service.Client;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+public class UsuarioClient {
+
+    private int idUsuario;
+    private String nomeUsuario;
+    private String emailUsuario;
+    private String telefoneUsuario;
+    private String cpfUsuario;
+
+}
